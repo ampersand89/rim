02f8f73072139c20752c8238d9cd39a1425fec77
ampersand89 <hbarria.cardenas@gmail.com>
2016-08-15 15:30:15 -0300

    Añadir nuevos archivos
    pre final

---
+++ helpers.py
@@ -0,0 +1,248 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+#============================================================================#
+# helpers.py, este archivo se utiliza para exportar funciones que recaban# 
+# infomacion general de la maquina objetivo                                  #
+# @HB.                                                                       #
+#============================================================================#
+import wmi
+
+
+#========================================================#
+#========================================================#
+# Seccion Clase Win32_PWin32_CDROMDrive                  #
+#========================================================#
+#========================================================#
+
+def cd(machine):
+    cds = []
+    for cd in machine.Win32_CDROMDrive():
+        cds.append(cd.Caption)
+        return cds
+#========================================================#
+#========================================================#
+# Seccion Clase Win32_BIOS                               #
+#========================================================#
+#========================================================#
+
+#Funcion Version de Bios
+def BiosVersion(machine):
+    BiosVersion = ""
+    algo = ""
+    for version in machine.Win32_BIOS():
+        bv = version.BiosVersion
+    for x in range(0, len(bv)):
+        BiosVersion+=algo.join(bv[x])
+    return str(BiosVersion)
+    
+def SerialNumber(machine):
+    """ Obtener número de serie """
+    for Serial in machine.Win32_BIOS():
+        Serie = Serial.serialnumber
+        return Serie
+    
+def Bios(machine):
+    dataBios = []
+    for B in machine.Win32_BIOS():
+        dataBios.append(B.Name)
+        dataBios.append(B.BiosCharacteristics) #create dict
+        dataBios.append(B.Manufacturer)
+        return dataBios
+        
+        
+        
+
+#========================================================#
+#========================================================#
+# Seccion Clase Win32_Processor              #
+#========================================================#
+#========================================================#      
+        
+#Funcion Nombre de Procesador 
+def Nom_proc(machine):
+        dataProcessor = []
+        for data in machine.Win32_Processor():
+                dataProcessor.append(data.Name) # [0]                     
+                dataProcessor.append(data.Status) # [1]
+                dataProcessor.append(data.CurrentClockSpeed)  # [2] Actual velocidad del reloj
+                dataProcessor.append(data.MaxClockSpeed)# [3] Maxima velocidad del reloj
+                dataProcessor.append(data.Family) # [4]
+                dataProcessor.append(data.L2CacheSize) # [5]
+                dataProcessor.append(data.L3CacheSize) # [6]
+                # Seleccion arquitectura [7]
+                if data.Architecture == 0:
+                        dataProcessor.append('x86')
+                elif data.Architecture == 1:
+                        dataProcessor.append('MIPS')
+                elif data.Architecture == 2:
+                        dataProcessor.append('Alpha')
+                elif data.Architecture == 3:
+                        dataProcessor.append('PowerPC')
+                elif data.Architecture == 5:
+                        dataProcessor.append('ARM')
+                elif data.Architecture == 6:
+                        dataProcessor.append('Itanium-based systems')
+                elif data.Architecture == 9:
+                        dataProcessor.append('x64')
+                else:
+                        dataProcessor.append('Unknown')
+
+                dataProcessor.append(data.Manufacturer) # [8] Fabricante
+                dataProcessor.append(data.NumberOfCores) # [9] Numero de Cores
+                dataProcessor.append(data.NumberOfLogicalProcessors)#[10] Procesadores Logicos
+                return dataProcessor # retorna una lista 
+        
+        
+#========================================================#
+#========================================================#
+# Seccion Clase Win32_ComputerSystem()           #
+#========================================================#
+#========================================================#  
+
+#Memoria Ram se transforma a formato MB para realizar   
+def memoriaram(machine):
+    for memory in machine.Win32_ComputerSystem():
+        MemoriaRam = int(memory.TotalPhysicalMemory)/1048576.0
+        return int(MemoriaRam) #FREE
+
+# Definimos marca de la maquina
+
+
+def marca(machine):
+    for marca in machine.Win32_ComputerSystem():
+        Marca = marca.Manufacturer
+        return Marca
+
+# Definimos modelo de la maquina
+def modelo(machine):
+    for modelo in machine.Win32_ComputerSystem():
+        Model = modelo.Model
+        return Model
+
+# Nombre de maquina
+
+def NameMachine(machine):
+    for nombre in machine.Win32_ComputerSystem():
+        NameMachine = nombre.Name
+        return NameMachine
+# usuario
+def DomainAndUsername(machine):
+    for u in machine.Win32_ComputerSystem():
+        user = u.username
+        return user
+
+#========================================================#
+#========================================================#
+# Seccion Clase Win32_OperatingSystem()                  #
+#========================================================#
+#========================================================#
+
+#Sistema Operativo
+def so(machine):
+    for version in machine.Win32_OperatingSystem():
+        so = version.Caption
+        OSA = version.OSArchitecture
+        ver = version.Version
+        SistemaOperativo = so + " " + OSA + " " + ver 
+        return SistemaOperativo
+
+
+#========================================================#
+#========================================================#
+# Seccion Clase Win32_DiskDrive()                        #
+#========================================================#
+#========================================================#
+
+
+#Datos de Harddisk
+
+def harddiskdata(machine):
+        harddiskinfo = []
+        for datos in machine.Win32_DiskDrive():
+                harddiskinfo.append(datos.Name) #[0]
+                harddiskinfo.append(datos.Model)#[1]
+                harddiskinfo.append(float(datos.Size)/(10.0**9))#[2]
+                harddiskinfo.append(datos.SerialNumber)#[3]
+                harddiskinfo.append(datos.TotalCylinders)#[4]
+                harddiskinfo.append(datos.TotalHeads)#[5]
+                harddiskinfo.append(datos.TotalSectors)#[6]
+                harddiskinfo.append(datos.TotalTracks)#[7]
+                harddiskinfo.append(datos.TracksPerCylinder)#[8]
+                harddiskinfo.append(datos.Status) #Status #[9]
+                harddiskinfo.append(datos.Partitions)#Numero de particiones [10]
+                return harddiskinfo
+                
+
+#========================================================#
+#========================================================#
+# Seccion Clase Win32_Baseboard()                        #
+#========================================================#
+#========================================================#
+                
+def baseboard(machine):
+    baseboard = []
+    for data in machine.Win32_Baseboard():
+        baseboard.append(data.Product)
+        baseboard.append(data.Manufacturer)
+        baseboard.append(data.Name)
+        baseboard.append(data.Model)
+        baseboard.append(data.Status)
+        return baseboard
+
+
+#========================================================#
+#========================================================#
+# Seccion Clase Win32_PhysicalMemory()                   #
+#========================================================#
+#========================================================#
+
+def MemoriaFisica(machine):
+    mfisica = []
+    for data in machine.Win32_PhysicalMemory():
+        mfisica.append(data.Caption) # [0]
+        mfisica.append(data.Capacity) # [1]
+        mfisica.append(data.DataWidth)# [2]
+        mfisica.append(data.DeviceLocator) #[3]
+        mfisica.append(data.FormFactor) #[4] complementa con diccionario
+        mfisica.append(data.InterleavePosition)#[5]
+        mfisica.append(data.Manufacturer)#[6]
+        mfisica.append(data.MemoryType) # [7] complementa con diccionario
+        mfisica.append(data.PartNumber) # [8]
+        mfisica.append(data.PositionInRow) # [9]
+        mfisica.append(data.SerialNumber)  # [10] Numero de serie correspondiente al dispositivo de memoria en SMBIOS INFORMACION
+        mfisica.append(data.Speed) # [11] Nanosegundos
+        mfisica.append(data.Tag) #  [12] Tag de la memoria
+        mfisica.append(data.TypeDetail) # [13] ver el tipo de memoria complementa con diccionario
+        return mfisica
+
+#========================================================#
+#========================================================#
+# Seccion Clase Win32_PhysicalMemoryArray                #
+#========================================================#
+#========================================================#
+
+def MatrizMemoriaFisica(machine):
+    mafisica = []
+    for data in machine.Win32_PhysicalMemoryArray():
+        mafisica.append(data.MemoryDevices)
+        if data.Use == 0:
+            mafisica.append('Reserved')
+        elif data.Use == 1:
+            mafisica.append('Other')
+        elif data.Use == 2:
+            mafisica.append('Unknown')
+        elif data.Use == 3:
+            mafisica.append('System Memory')
+        elif data.Use == 4:
+            mafisica.append('Video Memory')
+        elif data.Use == 5:
+            mafisica.append('Flash Memory')
+        elif data.Use == 6:
+            mafisica.append('Non-volatile RAM')
+        elif data.Use == 7:
+            mafisica.append('Cache Memory')
+        else:
+            mafisica.append('')
+        return mafisica
+                
--- main.py
+++ main.py
@@ -1,51 +1,308 @@
 #!/usr/bin/env python
-# -*- coding: cp1252 -*-
+# -*- coding: utf-8 -*-
+
+
 import wmi
 from recursos import helpers
 import os
+import sys
+import getpass
+import getopt 
+import socket
 
-class Machine(object):
-    def conexion(self):
-        self.ip=raw_input("IP: ")
-        # Se realiza la excepcion en caso de no haber conexion
+def usage():
+        print "Usage: rim <ip> <Arguments> <Output Arguments> \n" \
+        "\n" \
+        "Arguments: \n \n" \
+        "-h   --help \t \t - Show this help\n" \
+        "-u <username> \t - Specifies optional user name for login to remote computer.\n" \
+        "-p <password> \t\t - Specifies optional password for user name. \n\n" \
+        "-m <namemachine> \t - Specifies the hostname Windows\n" \
+        "-i <ip address> \t - Specifies the IP Address\n" \
+        "Output Arguments: \n\n" \
+        "-a --all \t\t - Show all Machine Information\n" \
+        "-j --hardware  \t\t - Show Hardware Information\n" \
+        "-s --software  \t\t - Show Software Information\n\n" \
+        "Examples:\n" \
+        "rim 192.168.0.10 --all\n" \
+        "rim 192.168.0.11 --hardware --software \n" \
+        "rim 192.168.0.20 \n" \
+
+def errorEnMismaMaquina():
+    print '''Error: You Must Specify ip or Hostname!
+RIM can be executed locally with the default credentials
+Try to execute RIM without arguments
+RIM has stopped!!!'''
+    sys.exit(2)
+
+def hardwareInformation(connect):
+    print '''
+Hardware Information Machine: %s\n
+Processor:\n
+
+Processor model\t\t= \t%s
+Status\t\t\t= \t%s
+Current Clock Speed\t= \t%s Mhz
+Max Clock Speed\t\t= \t%s Mhz
+Processor Family\t= \t%s 
+Level 2 Cache Size\t= \t%s KB
+Level 3 Cache Size\t= \t%s KB
+Architecture\t\t= \t%s
+Manufacturer\t\t= \t%s
+Number Of Cores\t\t= \t%s
+Logical Processor\t= \t%s
+
+HardDisk:\n
+
+HDD Name\t\t= \t%s
+HDD Model\t\t= \t%s
+HDD Size\t\t= \t%s GB
+HDD Serial Number\t= \t%s
+HDD Cylinders\t\t= \t%s
+HDD Heads\t\t= \t%s
+HDD Sectors\t\t= \t%s
+HDD Tracks\t\t= \t%s
+Tracks Per Cylinder\t= \t%s
+HDD Partition Numbers\t= \t%s
+
+Bios:\n
+
+Model\t\t\t= \t%s
+Version\t\t\t= \t%s
+Serial Number\t\t= \t%s
+Manufaturer\t\t= \t%s
+Characteristics\t\t= \t%s
+
+RAM: \n  
+
+Capacity RAM\t\t= \t%s MB
+Form Factor\t\t= \t%s
+Manufacturer\t\t= \t%s
+Memory Type\t\t= \t%s
+Part Number\t\t= \t%s
+Serial Number\t\t= \t%s
+Speed\t\t\t= \t%s Nanoseconds
+Tag\t\t\t= \t%s
+Type Detail\t\t= \t%s
+Slots\t\t\t= \t%s
+Memory Use\t\t= \t%s
+''' % (
+        helpers.NameMachine(connect),
+        helpers.Nom_proc(connect)[0],
+        helpers.Nom_proc(connect)[1],
+        helpers.Nom_proc(connect)[2],
+        helpers.Nom_proc(connect)[3],
+        helpers.Nom_proc(connect)[4],
+        helpers.Nom_proc(connect)[5],
+        helpers.Nom_proc(connect)[6],
+        helpers.Nom_proc(connect)[7],
+        helpers.Nom_proc(connect)[8],
+        helpers.Nom_proc(connect)[9],
+        helpers.Nom_proc(connect)[10],
+        helpers.harddiskdata(connect)[0],
+        helpers.harddiskdata(connect)[1],
+        int(helpers.harddiskdata(connect)[2]), #Aprox
+        helpers.harddiskdata(connect)[3],
+        helpers.harddiskdata(connect)[4],
+        helpers.harddiskdata(connect)[5],
+        helpers.harddiskdata(connect)[6],
+        helpers.harddiskdata(connect)[7],
+        helpers.harddiskdata(connect)[8],
+        helpers.harddiskdata(connect)[10],
+        helpers.Bios(connect)[0],
+        helpers.BiosVersion(connect),
+        helpers.SerialNumber(connect),
+        helpers.Bios(connect)[2],
+        helpers.Bios(connect)[1],
+        float(helpers.MemoriaFisica(connect)[1])/(1024*1024),
+        helpers.MemoriaFisica(connect)[4],
+        helpers.MemoriaFisica(connect)[6],
+        helpers.MemoriaFisica(connect)[7],
+        helpers.MemoriaFisica(connect)[8],
+        helpers.MemoriaFisica(connect)[10],
+        helpers.MemoriaFisica(connect)[11],
+        helpers.MemoriaFisica(connect)[12],
+        helpers.MemoriaFisica(connect)[13],
+        helpers.MatrizMemoriaFisica(connect)[0],
+        helpers.MatrizMemoriaFisica(connect)[1],
+        
+        
+        )
+
+def softwareInformation(connect):
+    print "software information"
+    pass
+
+def defaultInformation(connect):
+    print '''
+General Information:\n
+\nLogistics:\n
+Machine Name\t= \t%s
+Ip Address\t= \t%s
+Machine Brand\t= \t%s
+Machine Model\t= \t%s
+Serial Number\t= \t%s
+\nSummary:\n
+Operating System= \t%s 
+CPU\t\t= \t%s 
+RAM \t\t= \t%s MB
+Motherboard\t= \t%s
+HardDrives \t= \t%s
+Optical Drives\t= \t%s
+''' % (
+        helpers.NameMachine(connect), 
+        resolveByHostname(helpers.NameMachine(connect)), 
+        helpers.marca(connect), 
+        helpers.modelo(connect), 
+        helpers.SerialNumber(connect), 
+        helpers.so(connect), 
+        helpers.Nom_proc(connect)[0], 
+        str(helpers.memoriaram(connect)), 
+        helpers.baseboard(connect)[1] + " " + helpers.baseboard(connect)[0], 
+        helpers.harddiskdata(connect)[1], 
+        helpers.cd(connect)[0] 
+        )
+    sys.exit()
+
+def allInformation(connect):
+    print "mucha informacion"
+    pass
+
+def conexion(*args):
+
+    if (len(args) == 0):
         try:
-            self.machine = wmi.WMI(self.ip)
+            machine = wmi.WMI()
+            print "\n[*] Connected successfully..."
+            print "[*] Search Information..."
+            return machine
         except:
-            print "No fue posible realizar la conexión... \nVerifique si el host tiene conexión o si las credenciales tienen permisos para conectar"
+            print "[-] RIM isn't connected... \n" \
+            "[!] Verify if the host is up or if credentials are valid!"
+            sys.exit(2)
+    else:
+        try:
+            machine = wmi.WMI(computer=args[0], user=args[1], password=args[2])
+            print "\n[*] Connected successfully..."
+            print "[*] Search Information..."
+            return machine
+        except:
+            print "[-] RIM isn't connected... \n" \
+            "[!] Verify if the host is up or if credentials are valid!"
+            sys.exit(2)
+            
+def resolveByHostname(maquina):
+    try:
+        ip = socket.gethostbyname(maquina)
+        return ip
+    except socket.error:
+        print "[-] Error: Invalid Hostname"
+        sys.exit(2)
+        
+        
+def resolveByIpAddress(ip):
+    try:
+        maquina = socket.gethostbyaddr(ip)[0]
+        return maquina 
+    except socket.error:
+        print "[-] Error: Ip Address Not response to query"
+        sys.exit(2)
 
-# en cada metodo se debe utilizar self.machine para las clases wmi, que muestra una conexion establecida
-    def Hardware(self):
-        '''Se utiliza para recolectar datos de Hardware de la maquina'''
-        self.marca = helpers.marca(self.machine) # pasamos self.machine a los helpers para generar consulta
-        self.modelo = helpers.modelo(self.machine)
-        self.serie = helpers.SerialNumber(self.machine)
-        self.memoria_ram = helpers.memoriaram(self.machine)
-        self.harddiskdata= helpers.harddiskdata(self.machine)
 
-    def data_hardware(self):
-        print self.marca
-        print self.modelo
-        print self.serie
-        print self.memoria_ram
-        print self.harddiskdata[0]
+def main():
+    try:
+        opts, args = getopt.getopt(
+                                    sys.argv[1:], 
+                                    "hajsi:u:m:p:", 
+                                    [
+                                        "help", 
+                                        "all", 
+                                        "hardware",
+                                        "software",
+                                        "ip=", 
+                                        "user=",
+                                        "machine=",
+                                        "password=" ]
+                                        )
+    except getopt.GetoptError as err:
+        print str(err)
+        sys.exit(2)
 
-    def software(self):
-        '''se utiliza para recolectar datos de Software de la maquina'''
-        self.so = helpers.so(self.machine)
-        self.namemachine=helpers.NameMachine(self.machine)
-           
-  
-    def __init__(self):
-        self.conexion()
-        self.Hardware()
-        self.data_hardware()
+    if not opts:
+        print ("\n[!] No Option selected...\n[+] Executing RIM in the Same Machine...")
+        connect = conexion()
+        defaultInformation(connect)
+    
+
+    ip = None
+    user = None
+    maquina = None
+    password = None
+    todo = False
+    hw = False
+    sw = False
+    
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif opt in ("-i", "--ip"):
+            ip = arg
+        elif opt in ("-u", "--user"):
+            user = arg
+        elif opt in ("-m", "--machine"):
+            maquina = arg
+            if maquina.lower() == socket.gethostname().lower():
+                print "The Hostname is the same than this Machine"
+                errorEnMismaMaquina()
+            else:
+                pass
+        elif opt in ("-p", "--password"):
+            password = arg
+        elif opt in ("-a", "--all"):
+            todo = True
+        elif opt in ("-j", "--hardware"):
+            hw = True
+        elif opt in ("-s", "--software"):
+            sw = True
+        else:
+            assert False, "Error"
+            sys.exit(2)
+    
+    if not ip and not maquina:
+        if not user and not password:
+            pass
+    else:
+        if not ip:
+            ip = resolveByHostname(maquina)
+        if not maquina:
+            maquina = resolveByIpAddress(ip)
+        if not user:
+            user = helpers.DomainAndUsername(wmi.WMI())
+            password = getpass.getpass("Enter Password for %s : " % str(user))
+        if not password:
+            password = getpass.getpass("Enter Password for %s : " % str(user))
+        
+    connect = conexion(maquina, user, password)
+    if hw == True or sw == True:
+        if todo == True:
+                print "The option -a or --all can be executed without other output arguments"
+                sys.exit(2)
+        if hw == True:
+                print "[*] Collecting Hardware Information..."   
+                hardwareInformation(connect)
+        if sw == True:
+                print "[*] Collecting Software Information..."
+                softwareInformation(connect)
+    else:
+        if todo == True:
+                print "[*] Collecting Complete Information..."
+                allInformation(connect)
+        else:
+                print "[*] Collecting General Information..."
+                defaultInformation(connect)
+        
         
 
-
-# instanciar clase
-machine = Machine()
-
-
-
-
-
+if __name__ == "__main__":
+    main()
